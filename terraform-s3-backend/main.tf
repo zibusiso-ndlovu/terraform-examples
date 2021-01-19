31c230f0022a01444c1feaf172a303a4cd10328a
Zibusiso Edwin Ndlovu <ndlovuzibusiso@gmail.com>
2021-01-19 13:11:18 +0200

    Ädded Terraform S3 Backend Code

---
+++ terraform-s3-backend/main.tf
@@ -0,0 +1,71 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CREATE AN S3 BUCKET AND DYNAMODB TABLE TO USE AS A TERRAFORM BACKEND
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+
+    backend "s3" {
+    # Backend configuration block 
+    bucket         = "zibusiso-terraform-state-locking"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "zibusiso-locks"
+    encrypt        = true
+
+  }
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+# ------------------------------------------------------------------------------
+# CREATE THE S3 BUCKET
+# ------------------------------------------------------------------------------
+
+resource "aws_s3_bucket" "terraform_state" {
+  # TODO: change this to your own name! S3 bucket names must be *globally* unique.
+  bucket = "zibusiso-terraform-state-locking"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# ------------------------------------------------------------------------------
+# CREATE THE DYNAMODB TABLE
+# ------------------------------------------------------------------------------
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "zibusiso-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
